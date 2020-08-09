f729dd2e7b47556527b64f332666e199ff935c7b
Mahyar Azad <mhyrinc@gmail.com>
2020-08-09 22:17:33 +0300

    PORT fixed

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-
+const port = process.env.PORT;
 const DataStore = require('nedb');
 const database = new DataStore('database.db');
 database.loadDatabase();
 
-app.listen(3000, () => console.log('Listening at 3000'));
+app.listen(port, () => console.log('Listening at 3000'));
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
