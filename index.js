8b6f2f26b0bc898b75ca48c9f0b49d82c1777511
Mahyar Azad <mhyrinc@gmail.com>
2020-11-16 12:51:43 +0300

    Favicon syntax fixed

--- index.js
+++ index.js
@@ -72,6 +72,6 @@ app.use(passport.session());
 require('./config/passport')(app,passport);
 app.use(enforce.HTTPS({ trustProtoHeader: true }))
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/favicon.ico', express.static(path.join(__dirname, 'public')));
+app.use('/favicon.ico', express.static(path.join(__dirname, 'public','favicon.ico')));
 app.use('/user',require('./routes/user'));
 const db = require('./models/index.js');
