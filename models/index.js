ae21081bda53e47c373855c4556e0cafe1389c34
Maahyar Azad <39150387+Mahyarazad@users.noreply.github.com>
2021-05-22 18:03:51 +0430

    Update index.js

--- models/index.js
+++ models/index.js
@@ -4,31 +4,29 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-// const config = require(__dirname + '/../config/config.json')[env];
+//const env = process.env.NODE_ENV || 'development';
+const env = 'development';
+const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
-
-const config = {
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port:5432,
-  host: process.env.DB_HOST,
-  dialect: "postgres",
-  operatorsAliases: false,
-  URI:process.env.DB_URI,
-  define: {
-    createdAt: "createdat",
-    updatedAt: "updatedat"
-    }
-  };
 
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }},
+   define:config.define,
+   dialect: config.dialect
+  });
 }
+
+
 
 fs
   .readdirSync(__dirname)
